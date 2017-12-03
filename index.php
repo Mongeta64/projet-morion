d14f55d6eb24b8a0163a2e7d119d0853d42fa706
rominette <romaincouet34@gmail.com>
2017-12-03 20:16:23 +0100

    add style

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Title of the document</title>
+	<title>morpion war</title>
+	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" type="image" href="shadowgun-icone.jpg" />
 </head>
 
-<header class="head" ><span class="txt">morpion_war</span><a href="/"><img class="img"src="shadowgun-icone.jpg"/></a></header>
+<header class="head" ><span class="txt">morpion war</span><a class="home_link" href="/"><img class="img" src="shadowgun-icone.jpg"/></a></header>
 
-<nav>
-	<a class="menu_button" href="nouvelle_partie.php">nouvelle_partie</a>
-	<a class="menu_button" href="creer_une_equipe">creer_une_equipe</a>
-	<a class="menu_button" href="creer_un_morpion">creer_un_morpion</a>
+<nav class="menu">
+	<a class="menu_button" href="nouvelle_partie.php">nouvelle partie</a>
+	<a class="menu_button" href="creer_une_equipe">créer une équipe</a>
+	<a class="menu_button" href="creer_un_morpion">créer un morpion</a>
 </nav>
 
 <body>
